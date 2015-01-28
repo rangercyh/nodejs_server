75bde543ac4205736cf6bff0456e63ed453aeafa
rangercyh <rangercyh@qq.com>
2015-01-28 13:11:23 +0800

    add client tcp heart beat process

--- gate.js
+++ gate.js
@@ -6,6 +6,7 @@ var session = require('./session');
 var HOST = '10.20.127.197';
 var PORT = 8124;
 //var TIMEOUT = 10000;
+var CHECK_HEALTH_TIME = 5 * 60 * 1000;	// 检查心跳时间
 
 var server = net.createServer(function(socket) {
 	console.log('创建了一个新的socket连接：' + socket.remoteAddress + ' ' + socket.remotePort);
@@ -21,6 +22,22 @@ server.on('listening', function() {
 server.on('error', function(e) {
 	console.error('监听到一个错误：' + e);
 });
+
+/* 服务端心跳包维护
+心跳包由客户端发送，客户端有两种实现选择，可以直接每隔1min发送一个，
+也可以距离上一次正常数据发送后1min才开始发。两种实现对服务端都一样。
+
+服务端在收到心跳包或者正常数据时，都去更新socket列表的lastdata时间戳，
+这个时间使用服务器上的时间，因为不信任客户端的时间，然后服务器上设置定时器，
+每隔5min（时间可调）检查一下所有的socket列表lastdata，如果发现lastdata距离当前时间超过3min（时间可调），
+则认为客户端断线了。同时服务器对客户端的每条协议包括心跳包都需要立刻回复。
+
+客户端无论是发送正常数据还是心跳包，都开启个定时器检查，超时即认为和服务器断开连接，走重连处理
+*/
+setInterval(function() {
+	session.checkHealth();
+}, CHECK_HEALTH_TIME);
+
 
 
 
--- session.js
+++ session.js
@@ -6,6 +6,8 @@ var dispatcher = require('./dispatcher');
 
 var _sessionHandler = {};
 var _sessionid = 1;	// 逐渐递增，之后思考一种sessionid维护规则，可以回收淘汰的id
+var _HealthTime = 3 * 60 * 1000;	// 心跳超时时间
+
 
 
 function listenEvent(_sessionid) {
@@ -13,17 +15,19 @@ function listenEvent(_sessionid) {
 		var socket = _sessionHandler[_sessionid]._socket;
 		if (socket) {
 			// data use exbuffer
-			var exBuffer = new ExBuffer();
+			var exBuffer = new ExBuffer();	// 2bytes len + 2bytes msgid + data
 			exbuffer.on('data', function(buffer) {
 				var msgid = buffer.toString('base64', 2, 4);
 				dispatcher(buffer.toString('base64', 4), msgid, socket);
 			});
 			socket.on('data', function(data) {
 				exBuffer.put(data);
+				_sessionHandler[_sessionid]._lasthealth = Date.now();
 			});
 
 			socket.on('end', function() {
 				console.log('客户端发来了FIN');
+				_sessionHandler[_sessionid]._state = Const.session_state.SOCKET_STATE_CLIENT_END;
 			});
 
 			socket.on('error', function() {
@@ -34,7 +38,7 @@ function listenEvent(_sessionid) {
 				module.exports.destroy(_sessionid);
 			});
 
-			socket.setKeepAlive(true);
+			//socket.setKeepAlive(true);	// 不使用linux的keepalive，用心跳包代替
 			socket.setNoDelay(true);
 		}
 	}
@@ -51,57 +55,26 @@ module.exports.createSession = function(socket) {
 		_socket : socket,
 		_playerid : 0,
 		_playername : ""，
-		//_lasthealth : 心跳包的设计
+		_lasthealth : 0,
+		_connecttime : Date.now()
 	};
 
 	listenEvent(_sessionid);
-
-	// var exBuffer = new ExBuffer();	// 如何释放，默认是2个字节的len，big endian，所以一个包最长是65535字节
-	// exBuffer.on('data', function(buffer) {
-	// 	console.log('>> server receive data, length: ' + buffer.length);
-	// 	console.log(buffer.toString());
-
-	// 	var msgid = buffer.toString('base64', 2, 4);
-	// 	dispatcher(buffer.toString('base64', 4), msgid, socket);
-	// });
-
-	//var hellomsg = {
-	//	type:1,
-	//	data:'你好，这里是nodejs服务端'
-	//};
-	//socket.write(JSON.stringify(hellomsg));
-
-	// socket.on('data', function(data) {
-	// 	exBuffer.put(data);	// 收到数据就往exbuffer里丢
-	// });
-
-	// socket.on('end', function() {
-	// 	console.log('客户端发来FIN，转CLOSE_WAIT，等写队列发完了，回发FIN转LAST_ACK');
-	// 	console.log('或者从FIN_WAIT2状态转TIME_WAIT状态，发ack');
-	// });
-
-	// socket.on('error', function(exception) {
-	// 	console.error('socket error and close socket：' + exception);
-	// });
-
-	// socket.on('close', function() {
-	// 	console.log('socket该销毁了：');
-	// 	module.exports.destroy(_sessionid);
-	// });
-	// socket.setKeepAlive(true);
-	// socket.setNoDelay(true);	// 去掉nagle算法
-
-	/*
-	socket.setTimeout(TIMEOUT, function() {
-		console.log('连接超时，发送FIN收ack，转FIN_WAIT_2状态');
-		socket.end();
-		socket.setKeepAlive(true);
-	});	// 设置连接超时
-	*/
 };
+
+function checkDuplicates(playerid) {
+	for (id in _sessionHandler) {
+		if (_sessionHandler[id]._playerid == id) {
+			module.exports.destroy(id);
+		}
+	}
+}
 
 module.exports.bind = function(sessionid, playerid, playername) {
 	if (getSessionState(sessionid) == Const.session_state.SOCKET_STATE_IDLE) {
+		// 排重，只需要session排重，后端数据自己会处理重复
+		checkDuplicates(playerid);
+
 		_sessionHandler[sessionid]._playerid = playerid;
 		_sessionHandler[sessionid]._playername = playername;
 		_sessionHandler[sessionid]._state = Const.session_state.SOCKET_STATE_LOGIN;
@@ -126,3 +99,12 @@ module.exports.destroy = function(sessionid) {
 	delete _sessionHandler[sessionid];
 };
 
+module.exports.checkHealth = function() {
+	for (id in _sessionHandler) {
+		var curTime = Date.now();
+		if ((curTime - _sessionHandler[id]._lasthealth) > _HealthTime) {
+			// 通知数据模块清掉玩家数据
+			module.exports.destroy(id);
+		}
+	}
+};
